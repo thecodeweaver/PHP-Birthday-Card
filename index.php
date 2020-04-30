b9239fcbc2300ff13e8189e3bf8ab32f11b71be5
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2020-04-29 23:37:41 -0700

    Update centering

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
     $birthday_banner = <<< END
-    <style type="text/css">
-    .centered {
-        height: 200px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    </style>
 <pre>
 
 __   __  _______  _______  _______  __   __    _______         ______   _______  __   __    __   __  _______  __   __  __  
@@ -23,6 +15,14 @@ __   __  _______  _______  _______  __   __    _______         ______   _______ 
 END;
 
 $gif_html = <<< END
+<style type="text/css">
+    .centered {
+        height: 200px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    </style>
 <div class="centered">
 <iframe src="https://giphy.com/embed/3ohhwmQ0xIg8W3pHd6" width="480" height="456" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/happy-birthday-3ohhwmQ0xIg8W3pHd6">via GIPHY</a></p>
 </div>
