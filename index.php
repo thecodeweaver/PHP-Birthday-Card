5a290c51fc226dcc9cf45ae25a37660ea52b28aa
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2020-04-29 23:07:56 -0700

    Attempt to center the GIF

--- index.php
+++ index.php
@@ -15,7 +15,22 @@ __   __  _______  _______  _______  __   __    _______         ______   _______ 
 END;
 
 $gif_html = <<< END
+<style type="text/css">
+.firstDiv {
+    width: 500px;
+  
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  
+    background-color: #F1F1F1;
+   }
+</style>
+
+<div class="centered">
 <iframe src="https://giphy.com/embed/3ohhwmQ0xIg8W3pHd6" width="480" height="456" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/happy-birthday-3ohhwmQ0xIg8W3pHd6">via GIPHY</a></p>
+</div>
 END;
 
 echo "<head><title>Happy Birthday Mom!</title></head>";
