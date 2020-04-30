019e236efd5da229ff94d09075a7d6a393033a9f
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2020-04-29 23:39:11 -0700

    Reverted to old centering

--- index.php
+++ index.php
@@ -16,12 +16,16 @@ END;
 
 $gif_html = <<< END
 <style type="text/css">
-    .centered {
-        height: 200px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+centered {
+    width: 500px;
+  
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  
+    background-color: #F1F1F1;
+   }
     </style>
 <div class="centered">
 <iframe src="https://giphy.com/embed/3ohhwmQ0xIg8W3pHd6" width="480" height="456" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/happy-birthday-3ohhwmQ0xIg8W3pHd6">via GIPHY</a></p>
