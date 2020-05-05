4adf0cf535581ae8d0253d934bae922a03a7346f
thecodeweaver <44348986+thecodeweaver@users.noreply.github.com>
2020-05-05 14:05:50 -0700

    Update index.php

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $format = "json";
 
 try {
     $result = $api_client->gifsRandomGet($api_key, $query, $rating, $format);
-    print_r($result->$data);
+    print_r($result->data);
 
     # echo "<img src=\"$url\" alt=\"Random Birthday GIF\">";
 } catch (Exception $e) {
