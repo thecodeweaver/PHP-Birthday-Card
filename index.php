8639fec74c204d90cd27be2733982ca3a0ebb8de
TheCodeWeaver <thecodeweaver@protonmail.com>
2020-05-06 00:37:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,8 @@ $format = "json";
 try {
     $result = $api_client->gifsRandomGet($api_key, $query, $rating, $format);
     print_r($result);
-
+    
+    print_r($result["data"]);
     # echo "<img src=\"$url\" alt=\"Random Birthday GIF\">";
 } catch (Exception $e) {
     echo "Error loading GIF: " . $e->getMessage() . "\n";
